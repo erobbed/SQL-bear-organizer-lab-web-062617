d7ececf943e80d36f870d0666419c75146651594
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-07-04 21:31:36 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears VALUES
+(1, "Mr. Chocolate", 20, "M", "brown", "shy", 1),
+(2, "Rowdy", 10, "F", "brown", "friendly", 0),
+(3, "Tabitha", 6, "F", "black", "grumpy", 0),
+(4, "Sergeant Brown", 7, "F", "brown", "friendly", 1),
+(5, "Melissa", 13, "F", "black", "grumpy", 0),
+(6, "Grinch", 2, "M", "black", "grumpy", 0),
+(7, "Wendy", 6, "F", "brown", "shy", 0),
+(8, null, 10, "M", "black", "aggressive", 0);
